1e3bef5366ec8b18b69891fea548c95173e629b2
katakamlokesh <lokeshkatakam3@gmail.com>
2021-08-10 08:53:23 +0000

    A

---
+++ index.js
@@ -0,0 +1,14 @@
+const element = (
+//   Write your code here.
+    const element = (
+    <div className="bg-container">
+        <h1 className="heading">Super Over League</h1>
+        <div className="team-container">
+            <img className="team-image" src="https://assets.ccbp.in/frontend/react-js/rcb-img.png/"/>
+            <img className="team-image" src="https://assets.ccbp.in/frontend/react-js/csk-img.png"/>
+        </div>
+    </div>
+    );
+)
+
+ReactDOM.render(element, document.getElementById('root'))
